327dfd1c587c11d77540533a1d30f4736e132b55
Ming Duan <11203duan@gmail.com>
2019-05-23 00:03:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,21 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[pirate])
+      binding.pry
+      @ship_1 = Ship.new(params[pirate][ships][0])
+      @ship_2 = Ship.new(params[pirate][ships][1])
+      erb :show
+    end
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,12 @@
 class Ship
+
+attr_reader :name, :type, :booty
+
+def initialize(hash)
+  @name = hash[:name]
+  @type = hash[:type]
+  @booty = hash[:booty]
+end
+
+
 end
